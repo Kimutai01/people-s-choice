1c1c78e272c069e111bfac0c2073e5d96721b24c
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-07-28 15:15:06 +0300

    People's choice

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="home">
+      <h1>Hello There, we are building something awesome!</h1>
+    </div>
+  );
+}
+
+export default App;
