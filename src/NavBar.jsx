52dc0d49cf9089d1d56d9fc84facf84aece730d8
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-08-08 16:40:39 +0300

    Setup

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,8 +3,16 @@ import React from "react";
 const NavBar = () => {
   return (
     <div>
-      <div className="flex bg-black justify-between items-center p-4 text-white w-[100%]">
-        <div className="md:w-[40%] w-[25%]  gap-4 flex flex-col ">
+      <div className="flex bg-black justify-between items-start p-4 text-white w-[100%]">
+        <div className="md:w-[40%] w-[25%] h-[50px] gap-4 flex justify-end flex-col ">
+          <div className="flex gap-2">
+            <p>Home</p>
+            <p>About Us</p>
+            <p>Sponsorship</p>
+            <p>Gallery</p>
+            <p>FAQs</p>
+          </div>
+
           <p className="bg-[#BF8134] h-[2px] w-[100%]"></p>
         </div>
 
@@ -16,7 +24,13 @@ const NavBar = () => {
           />
         </div>
 
-        <div className="md:w-[40%] w-[25%]  flex flex-col items-end">
+        <div className="md:w-[40%] w-[25%] text-base h-[50px] gap-4  flex justify-end items-end flex-col ">
+          <div className="flex gap-2">
+            <p>Register</p>
+            <p>|</p>
+            <p>Vote</p>
+          </div>
+
           <p className="bg-[#BF8134] h-[2px] w-[100%]"></p>
         </div>
       </div>
