07ae7f2559b9b8f7a8c514cf43fd0ab75c3b258a
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-08-08 16:58:50 +0300

    Setup

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose, AiOutlineMail } from "react-icons/ai";
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+  const toggleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div>
-      <div className="flex bg-black justify-between items-start p-4 text-white w-[100%]">
-        <div className="md:w-[40%] w-[25%] h-[50px] gap-4 flex justify-end flex-col ">
-          <div className="flex gap-2">
+      <div className="flex bg-black justify-between  md:flex-row flex-row-reverse items-start p-4 text-white w-[100%]">
+        <div className="md:w-[40%] w-[25%] h-[50px] gap-4 flex justify-end items-end flex-col ">
+          <div className=" hidden md:flex gap-2">
             <Link to="/">
               <p className="hover:scale-105 transition-all ease-in-out duration-500 hover:text-[#BF8134]">
                 Home
@@ -33,6 +40,19 @@ const NavBar = () => {
               </p>
             </Link>
           </div>
+          <div className="md:hidden block z-10">
+            {nav ? (
+              <AiOutlineClose
+                className=" text-white text-2xl"
+                onClick={toggleNav}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-white text-2xl"
+                onClick={toggleNav}
+              />
+            )}
+          </div>
 
           <p className="bg-[#BF8134] h-[2px] w-[100%]"></p>
         </div>
@@ -45,7 +65,7 @@ const NavBar = () => {
           />
         </div>
 
-        <div className="md:w-[40%] w-[25%] text-base h-[50px] gap-4  flex justify-end items-end flex-col ">
+        <div className="md:w-[40%] w-[25%] md:text-base text-xs h-[50px] gap-4  flex justify-end items-end flex-col ">
           <div className="flex gap-2">
             <p>Register</p>
             <p>|</p>
@@ -54,6 +74,33 @@ const NavBar = () => {
 
           <p className="bg-[#BF8134] h-[2px] w-[100%]"></p>
         </div>
+      </div>
+      <div
+        className={
+          nav
+            ? "sm:hidden absolute top-0 right-0 left-0 bottom-0 flex justify-center items-center text-center  w-full h-screen bg-black/80 text-white duration-300 ease-in-out"
+            : "sm:hidden absolute top-0 right-0 left-[-100%] bottom-0 flex justify-center items-center text-center w-full h-screen  bg-black/80 text-white duration-300 ease-in-out"
+        }
+      >
+        <ul onClick={toggleNav}>
+          <Link to="/">
+            <li className="p-4">Home</li>
+          </Link>
+
+          <Link to="/about">
+            <li className="p-4">About Us</li>
+          </Link>
+
+          <Link to="/sponsorship">
+            <li className="p-4">Sponsorship</li>
+          </Link>
+          <Link to="/gallery">
+            <li className="p-4">Gallery</li>
+          </Link>
+          <Link to="/faqs">
+            <li className="p-4">FAQs</li>
+          </Link>
+        </ul>
       </div>
     </div>
   );
