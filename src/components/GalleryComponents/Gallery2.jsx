613144c2aa89c14eb945c68b132d6b7be71e334f
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-08-08 14:31:00 +0300

    added about

--- src/components/GalleryComponents/Gallery2.jsx
+++ src/components/GalleryComponents/Gallery2.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
 
 const Gallery2 = () => {
   return (
-    <div>Gallery2</div>
-  )
-}
+    <div className="flex  h-[100vh]">
+      <div className="w-1/3 h-[100vh]">
+        <div className="h-1/2 w-[100%]">
+          <img
+            src="/homebg.png"
+            alt=""
+            className="w-[100%] h-[100%] object-cover"
+          />
+        </div>
 
-export default Gallery2
+        <div className="h-1/2  w-[100%] ">
+          <img
+            src="/homebg.png"
+            alt=""
+            className="w-[100%] h-[100%] object-cover"
+          />
+        </div>
+      </div>
+      <div className="w-1/3 h-[100vh]">
+        <div className="h-[100%] w-[100%] ">
+          <img
+            src="/homebg.png"
+            alt=""
+            className="w-[100%] h-[100%] object-cover"
+          />
+        </div>
+      </div>
+      <div className="w-1/3 h-[100vh]">
+        <div className="h-1/3  w-[100%]">
+          <img
+            src="/homebg.png"
+            alt=""
+            className="w-[100%] h-[100%] object-cover"
+          />
+        </div>
+        <div className="h-1/3  w-[100%] ">
+          <img
+            src="/homebg.png"
+            alt=""
+            className="w-[100%] h-[100%] object-cover"
+          />
+        </div>
+        <div className="h-1/3  w-[100%]">
+          <img
+            src="/homebg.png"
+            alt=""
+            className="w-[100%] h-[100%] object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery2;
