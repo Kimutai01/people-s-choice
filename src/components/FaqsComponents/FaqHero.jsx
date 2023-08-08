3b6e461e5d45d45ef312cb0637a5fcafb622da9b
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-08-08 17:13:52 +0300

    Responsive

--- src/components/FaqsComponents/FaqHero.jsx
+++ src/components/FaqsComponents/FaqHero.jsx
@@ -3,6 +3,12 @@ import React from "react";
 const FaqHero = () => {
   return (
     <div className="bg-[#000]">
+      <div className="flex items-center mx-auto md:gap-10 gap-5 w-[90%] md:w-[80%] pb-10">
+        <div className="border-t-[1px] border-gray-400 md:w-[20%] w-[10%]"></div>
+        <h1 className="text-[#fff] uppercase text-lg md:text-5xl font-bold md:font-extrabold">
+          FREQUENTLY ASKED QUESTIONS
+        </h1>
+      </div>
       <div className="relative w-[80%] mx-auto">
         <div
           className="absolute inset-0"
@@ -17,7 +23,7 @@ const FaqHero = () => {
           alt="Your Image"
         />
         <div className="absolute bottom-0 left-0 w-full text-center p-4">
-          <p className="text-white font-extrabold text-9xl">FAQS</p>
+          <p className="text-white font-extrabold text-5xl md:text-9xl">FAQS</p>
         </div>
       </div>
     </div>
