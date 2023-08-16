c074b3d4ff6bb9fa94f3189832387f3a51c94460
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-08-16 10:43:34 +0300

    Add

--- src/components/FaqsComponents/SendMessage.jsx
+++ src/components/FaqsComponents/SendMessage.jsx
@@ -1,8 +1,71 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 const SendMessage = () => {
+  const form = useRef();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if (!firstName || !lastName || !email || !message) {
+      toast.error("Email not sent ,Please fill in all fields", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    } else {
+      emailjs
+        .sendForm(
+          "service_bm094u7",
+          "template_bwh4jns",
+          form.current,
+          "iClOpEkVpF03NEqNf"
+        )
+        .then(
+          (result) => {
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+
+            setMessage("");
+            console.log(result);
+
+            setTimeout(() => {
+              toast.success("Email Sent , We will get back to you soon", {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+              });
+            }, 1000);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
+  };
+
   return (
     <div className="border-b-[#BF8134]  border-2 border-l-0 border-r-0 border-t-[#BF8134]">
+      <ToastContainer />
       <div className="flex flex-col w-[100%] gap-8 bg-black p-8">
         <div className="flex justify-between items-center p-4">
           <div className="md:w-[5%]">
@@ -15,12 +78,19 @@ const SendMessage = () => {
           </div>
         </div>
         <div className="flex items-end justify-end  text-white">
-          <div className="md:w-[50%] flex flex-col gap-2">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="md:w-[50%] flex flex-col gap-2"
+          >
             <div className="flex justify-between w-[100%]">
               <div className="w-[48%]">
                 <input
                   type="text"
                   placeholder="First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  name="first_name"
                   className="w-[100%] h-[50px]  bg-[#BF8134] focus:border-none focus:border-transparent border-0 border-transparent placeholder-[#CBCCD3] text-[#CBCCD3] outline-none ring-0 border-none rounded-md p-4"
                 />
               </div>
@@ -28,6 +98,9 @@ const SendMessage = () => {
                 <input
                   type="text"
                   placeholder="Last Name"
+                  name="last_name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
                   className="w-[100%] h-[50px]  bg-[#BF8134] focus:border-none focus:border-transparent border-0 border-transparent placeholder-[#CBCCD3] text-[#CBCCD3] outline-none ring-0 border-none rounded-md p-4"
                 />
               </div>
@@ -36,6 +109,9 @@ const SendMessage = () => {
               <input
                 type="text"
                 placeholder="Email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 className="w-[100%] h-[50px]  bg-[#BF8134] focus:border-none focus:border-transparent border-0 border-transparent placeholder-[#CBCCD3] text-[#CBCCD3] outline-none ring-0 border-none rounded-md p-4"
               />
             </div>
@@ -43,16 +119,21 @@ const SendMessage = () => {
               <textarea
                 type="text"
                 placeholder="Write Your Message Here"
+                value={message}
+                name="message"
+                onChange={(e) => setMessage(e.target.value)}
                 className="w-[100%] h-[200px]  bg-[#BF8134] focus:border-none focus:border-transparent border-0 border-transparent placeholder-[#CBCCD3] text-[#CBCCD3] outline-none ring-0 border-none rounded-md p-4"
               />
             </div>
             <div className="w-[100%] flex md:justify-end">
-
-              <button className=" w-[226px] h-[57px] text-2xl uppercase bg-white rounded-3xl text-[#BF8134]">
+              <button
+                className=" w-[226px] h-[57px] text-2xl uppercase bg-white rounded-3xl text-[#BF8134]"
+                type="submit"
+              >
                 Send Message
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
